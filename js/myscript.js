b43780852185443e80c68b26464cb9aa992f39a9
project-engineer <plant.engineer@heavenlyblush.com>
2026-05-02 02:02:10 +0700

    Home sudah lumayan tinggal ditambahakan hal hal kecil, halaman PMT sudah ada dua halaman, tombol offcanvas menu sudah memakai icon

---
+++ js/myscript.js
@@ -0,0 +1,7 @@
+function bestpack() {
+    document.getElementById("konten").innerHTML = '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdnFbQkifXrnRKuHdyI2yRY6JiiV8qyrKQjiufsuPBOvNqooA/viewform?embedded=true" width="640" height="3739" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>'
+}
+
+function powerpack() {
+    document.getElementById("konten").innerHTML = '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSck3oHzqQvuBHo3Gusrr4sME6oiY0tHSlugV1_eRQPluZmZtw/viewform?embedded=true" width="640" height="3991" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>'
+}
